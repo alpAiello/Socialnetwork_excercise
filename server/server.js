5162dc9eb781d242fd55a8546d1a8b563fb28166
Sandy-HZL <4lessandro.aiello@gmail.com>
2021-03-07 17:17:08 +0100

    added form in register component and added babel... to make arrow function work

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const compression = require("compression");
 const path = require("path");
-const bcrypt = require("./bcrypt");
 const app = express();
-const db = require("./sql/database.js");
+// const db = require("./sql/database.js");
 const cookieSession = require("cookie-session");
+const auth = require("./routes/auth.js");
 
 app.use(
 	cookieSession({
@@ -24,61 +24,15 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
-app.post("/api/users/register", (req, res) => {
-	console.log(req.body);
-	const { username, firstName, lastName, email, password } = req.body;
-	console.log(username, firstName, lastName, email, password);
-	bcrypt
-		.genHash(password)
-		.then((hashed_password) => {
-			console.log(hashed_password);
-			db.addUser(username, firstName, lastName, email, hashed_password)
-				.then((dbReturnData) => {
-					const userData = dbReturnData.rows[0];
-					console.log(dbReturnData.rows[0]);
-					const {
-						username,
-						firstname,
-						lastname,
-						email,
-						hashed_password,
-					} = userData;
-					console.log(
-						"The user was added successfully",
-						"data:",
-						username,
-						"data:",
-						firstname,
-						"data:",
-						lastname,
-						"data:",
-						email,
-						"data:",
-						hashed_password
-					);
-					req.session.user = userData;
-					res.json({
-						registrationMessage: "registration successfully",
-					});
-				})
-				.catch((err) => {
-					console.log(
-						"must make a real error message here !!!!",
-						err
-					);
-					res.json({
-						registrationMessage: "registration failed",
-					});
-				});
-		})
-		.catch((err) => {
-			console.log("must make a real error message here !!!!", err);
-			res.json({ registrationMessage: "Password encryption failed" });
-		});
-});
+app.use("/api/auth", auth);
 
 app.get("*", function (req, res) {
+	// if (!req.session.user) {
+	// 	res.redirect(302, "/welcome");
+	// } else {
+	// 	res.redirect(302, "/");
 	res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+	// }
 });
 
 app.listen(process.env.PORT || 3001, function () {
