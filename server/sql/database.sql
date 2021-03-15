79e442b889a005dfe728038a0667d84b066c42f8
Sandy-HZL <4lessandro.aiello@gmail.com>
2021-03-15 15:36:06 +0100

    added new data table friends_requests

--- server/sql/database.sql
+++ server/sql/database.sql
@@ -3,23 +3,34 @@ DROP TABLE IF EXISTS reset_codes;
 
 CREATE TABLE IF NOT EXISTS users
 	(
-id serial unique PRIMARY KEY ,
-username varchar(256),
-firstname varchar(256) NOT NULL,
-lastname varchar(256) NOT NULL,
-profile_picture_url varchar(256),
-email varchar(256) UNIQUE NOT NULL,
-hashed_password varchar(256) NOT NULL,
-bio varchar(256),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id serial unique PRIMARY KEY ,
+    username varchar(256),
+    firstname varchar(256) NOT NULL,
+    lastname varchar(256) NOT NULL,
+    profile_picture_url varchar(256),
+    email varchar(256) UNIQUE NOT NULL,
+    hashed_password varchar(256) NOT NULL,
+    bio varchar(256),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
      );
 
 CREATE TABLE if not EXISTS reset_codes
     (
-        id serial unique PRIMARY KEY ,
-        email varchar(256) NOT NULL,
-        reset_code varchar(256) UNIQUE NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id serial unique PRIMARY KEY ,
+    email varchar(256) NOT NULL,
+    reset_code varchar(256) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
      );
+
+CREATE TABLE if not EXISTS friend_requests
+(
+    id serial unique PRIMARY KEY ,
+    from_id varchar(256),
+    to_id varchar(256),
+    accepted varchar(256),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 SELECT * FROM users;
 SELECT * FROM reset_codes;
+SELECT * FROM friend_requests
